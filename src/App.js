54fcca4b999f5d0eca235a26179c9fdd9c31992e
Yordan <yordan_yordanov001@abv.bg>
2022-01-13 17:58:05 +0200

    useState and onChange Handler added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import "./App.css";
+import React, { useState, useMemo } from "react";
 
 function App() {
+  const [number, setNumber] = useState(0);
+
+  const onChange = (e) => {
+    setNumber(e.currentTarget.value);
+    console.log(number);
+  };
+
   return (
     <div className="App">
-     <div className="control has-icons-right">
+      <div className="control has-icons-right">
         <input
           className="input is-large"
           type="text"
           placeholder="Enter number..."
-
+          value={number}
+          onChange={onChange}
         />
         <span className="icon is-small is-right">
           <i className="fas fa-times" />
-        </span> 
+        </span>
       </div>
     </div>
   );
